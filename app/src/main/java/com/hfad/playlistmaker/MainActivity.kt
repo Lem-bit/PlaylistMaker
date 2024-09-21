6929a762faad63121ec2229daec5d44c731e6d96
l.ansperi <desa-07@mail.ru>
2024-09-21 21:51:00 +0300

    Новый проект

--- app/src/main/java/com/hfad/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/hfad/playlistmaker/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.hfad.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<Button>(R.id.btn_search).setOnClickListener {
+            startActivity(Intent(this, SearchActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btn_media).setOnClickListener {
+            startActivity(Intent(this, MediaActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btn_settings).setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+
     }
+
 }

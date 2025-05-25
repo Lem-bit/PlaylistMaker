6e5e6548644be5b1f9dd35fad89f87e69f1aef32
l.ansperi <desa-07@mail.ru>
2025-05-25 18:59:45 +0300

    14 спринт

--- app/src/main/java/com/hfad/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/hfad/playlistmaker/PlayerActivity.kt
@@ -1,9 +1,13 @@
 package com.hfad.playlistmaker
 
 import android.content.Context
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.TypedValue
 import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -17,6 +21,13 @@ class PlayerActivity : AppCompatActivity() {
     private var _binding: ActivityPlayerBinding? = null
     private val binding get() = _binding ?: throw IllegalStateException("Error binding ActivityPlayerBinding is null!")
 
+    private lateinit var playButton: ImageButton
+    private var mediaPlayer = MediaPlayer()
+    private var playerState = STATE_DEFAULT
+    private lateinit var url: String
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var updateProgressRunnable: Runnable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityPlayerBinding.inflate(layoutInflater)
@@ -27,6 +38,7 @@ class PlayerActivity : AppCompatActivity() {
         }
 
         val track = Gson().fromJson(intent.getStringExtra("track"), Track::class.java)
+        url = track.previewUrl
 
         with(binding) {
             trackName.text = track.trackName
@@ -44,10 +56,87 @@ class PlayerActivity : AppCompatActivity() {
                 .transform(RoundedCorners(dpToPx(8f, albumCover.context)))
                 .into(albumCover)
         }
+
+        preparePlayer()
+        playButton = binding.playBtn
+        playButton.setOnClickListener {
+            playbackControl()
+        }
+
+        updateProgressRunnable = object : Runnable {
+            override fun run() {
+                if (mediaPlayer.isPlaying) {
+                    binding.listenProgress.text = SimpleDateFormat(
+                        "mm:ss",
+                        Locale.getDefault()
+                    ).format(mediaPlayer.currentPosition)
+                    handler.postDelayed(this, TIMER_UPDATE_TIME)
+                }
+            }
+        }
     }
 
     private fun dpToPx(dp: Float, context: Context): Int {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics).toInt()
+    }
+
+    private fun preparePlayer() {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playButton.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playButton.setImageResource(R.drawable.ic_play)
+            binding.listenProgress.text = "0:00"
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playButton.setImageResource(R.drawable.ic_pause)
+        playerState = STATE_PLAYING
+        handler.post(updateProgressRunnable)
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playButton.setImageResource(R.drawable.ic_play)
+        playerState = STATE_PAUSED
+        handler.removeCallbacks(updateProgressRunnable)
+    }
+
+    private fun playbackControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+        handler.removeCallbacks(updateProgressRunnable)
+    }
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+        private const val TIMER_UPDATE_TIME = 500L
     }
 
 }
